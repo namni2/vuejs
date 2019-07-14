25ee3d0821895604e2f5d15c8cdcf93713789a3f
LeeNamHeun <namheun@naver.com>
2019-07-14 17:56:12 +0900

    뷰 api 테스트 가라데이터

--- src/template/body.vue
+++ src/template/body.vue
@@ -1,10 +1,33 @@
 <template>
-    <div>이쪽에서 api를 호출하여 ! 데이터를 가져올거에요~</div>
+    <div>
+        <button v-on:click="searchTerm">글 읽어오기</button>
+        <div>
+            <ul id="posts">
+                <li v-for="item in post">{{ item.title }}</li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'bodyHtml'
+  ,data:function(){
+      return {
+          post:[]
+      }
+  }
+  ,axios
+  ,methods:{
+      searchTerm:function(){
+          axios.get('https://jsonplaceholder.typicode.com/posts')
+          .then((result)=>{
+            console.log(result.data);
+            this.post = result.data;
+          })
+      }
+  }
 }
 </script>
 
