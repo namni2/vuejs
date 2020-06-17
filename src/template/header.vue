9d3f8a485335d1de567a7456eadf9d817da5bf08
LeeNamHeun <namheun@naver.com>
2020-06-17 23:52:57 +0900

    push for hosting -3

--- src/template/header.vue
+++ src/template/header.vue
@@ -21,7 +21,7 @@
         </div>
       </a>
 
-    <a href="/" v-if="current!=null && currentDate!=''">
+    <a href="/vuejs" v-if="current!=null && currentDate!=''">
         <svg
             stroke="currentColor"
             fill="currentColor"
